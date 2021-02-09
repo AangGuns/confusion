76f07add54116a1cef449fdc1dcf7ee938c7d33d
AangGuns <anggun.1998@students.amikom.ac.id>
2021-02-09 07:54:47 +0700

    component part one

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponent';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">Ristorante Con Fu</NavbarBrand>
         </div>
       </Navbar>
+      {/* menu component */}
+      <Menu />
     </div>
   );
 }
